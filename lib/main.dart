b85e6588fd0dfef42316b8d54d08332a0e45695f
Anna-K-S <anna.sergeevna1622@gmail.com>
2023-09-20 17:58:47 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(
+      const Todoey(),
+    );
+
+class Todoey extends StatelessWidget {
+  const Todoey({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+     
+    );
+  }
+}
